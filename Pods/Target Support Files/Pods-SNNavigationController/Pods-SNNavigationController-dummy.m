8b58dbe7e0cadcb5adbccdb597791932e9052cde
snlo <snloveydus@sina.com>
2019-04-03 17:08:45 +0800

    update

--- Pods/Target Support Files/Pods-SNNavigationController/Pods-SNNavigationController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SNNavigationController : NSObject
-@end
-@implementation PodsDummy_Pods_SNNavigationController
-@end
--- Pods/Target Support Files/Pods-SNNavigationControllerUITests/Pods-SNNavigationControllerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SNNavigationControllerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SNNavigationControllerUITests
-@end
